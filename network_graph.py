733431ae3a36ab752a57511bc8979983cdc524ae
LSaldyt <lucassaldyt@gmail.com>
2022-04-16 15:54:11 -0700

    Final visualizations

--- network_graph.py
+++ network_graph.py
@@ -2,6 +2,7 @@ import plotly.graph_objects as go
 import networkx as nx
 from utils import *
 import white_theme
+from pprint import pprint
 
 
 def plot_g(G):
@@ -13,8 +14,8 @@ def plot_g(G):
         edge_x.append(x0)
         edge_x.append(x1)
         edge_x.append(None)
-        edge_y.append(y0)
-        edge_y.append(y1)
+        edge_y.append(1-y0)
+        edge_y.append(1-y1)
         edge_y.append(None)
 
     edge_trace = go.Scatter(
@@ -28,12 +29,25 @@ def plot_g(G):
     for node in G.nodes():
         x, y = G.nodes[node]['pos']
         node_x.append(x)
-        node_y.append(y)
+        node_y.append(1-y)
+
+    node_text = []
+    node_depths = []
+    first = None
+    for node in G:
+        if first is None:
+            first = node
+        node_text.append(G.nodes[node]['text'])
+        # Inefficient but these are small graphs
+        try:
+            node_depths.append(nx.shortest_path_length(G, source=first, target=node))
+        except:
+            node_depths.append(1)
 
     node_trace = go.Scatter(
         x=node_x, y=node_y,
-        mode='markers+text',
-        # hoverinfo='text',
+        mode='markers',
+        text=node_text,
         marker=dict(
             showscale=True,
             # colorscale options
@@ -42,38 +56,51 @@ def plot_g(G):
             #'Hot' | 'Blackbody' | 'Earth' | 'Electric' | 'Viridis' |
             colorscale='YlGnBu',
             reversescale=True,
-            color=[],
-            size=40,
+            color=node_depths,
+            size=200,
             colorbar=dict(
                 thickness=15,
-                title='Node Connections',
+                title='Depth',
                 xanchor='left',
                 titleside='right'
             ),
             line_width=2))
 
-    node_adjacencies = []
-    node_text = []
-    for node, adjacencies in enumerate(G.adjacency()):
-        node_adjacencies.append(len(adjacencies[1]))
-        node_text.append(G.nodes[node]['text'])
+    annotations = []
+    # annotations=[
+    #     dict(
+    #     text='',
+    #     showarrow=False,
+    #     xref="paper", yref="paper",
+    #     x=0.005, y=-0.002 )]
 
-    node_trace.marker.color = node_adjacencies
-    node_trace.text = node_text
+    for x, y, t in zip(node_x, node_y, node_text):
+        color = 'black'
+        annotations.append(dict(
+            x=x, y=y, text=t,
+            xanchor='center',
+            yanchor='middle',
+            align='center',
+            xref='x',
+            yref='y',
+            font=dict(color=color, size=40),
+            showarrow=False, arrowhead=1
+            ))
 
     fig = go.Figure(data=[edge_trace, node_trace],
                  layout=go.Layout(
-                    title='<br>Network graph made with Python',
+                    title='',
                     titlefont_size=16,
                     showlegend=False,
                     hovermode='closest',
+                    annotations=annotations,
                     margin=dict(b=20,l=5,r=5,t=40),
                     xaxis=dict(showgrid=False, zeroline=False, showticklabels=False),
                     yaxis=dict(showgrid=False, zeroline=False, showticklabels=False))
                     )
-    fig.show()
+    return fig
 
-G = nx.random_geometric_graph(200, 0.125)
-names = ['a'] * 100 + ['b'] * 100
-nx.set_node_attributes(G, {k : {'text' : n} for k, n in enumerate(names)})
-plot_g(G)
+# G = nx.random_geometric_graph(200, 0.125)
+# names = ['a'] * 100 + ['b'] * 100
+# nx.set_node_attributes(G, {k : {'text' : n} for k, n in enumerate(names)})
+# plot_g(G)
--- plot.py
+++ plot.py
@@ -2,6 +2,7 @@ import plotly.graph_objects as go
 import plotly.express as px
 import plotly
 import pandas as pd
+from network_graph import nx, plot_g
 
 from utils import *
 
@@ -34,15 +35,78 @@ def visualize(l, legend_title='', title='', filename='diff'):
     save(fig, filename, w=1400, h=700)
     return fig
 
+def get_unique_key(k, seen):
+    i = 0
+    sub_k = f'{k}_{i}'
+    while sub_k in seen:
+        sub_k = f'{k}_{i}'
+        i += 1
+    seen.add(sub_k)
+    return sub_k
+
+def create_graph(topology, G=None, seen=None):
+    if not isinstance(topology, dict):
+        return topology
+    if G is None:
+        G = nx.MultiGraph()
+    if seen is None:
+        seen = set()
+    for k, v in topology.items():
+        k = get_unique_key(k, seen)
+        if isinstance(v, tuple):
+            for sv in v:
+                H = create_graph(sv, seen=seen)
+                if isinstance(H, nx.MultiGraph):
+                    G = nx.compose(G, H)
+                if isinstance(sv, dict):
+                    for sk in sv:
+                        sk = get_unique_key(sk, seen)
+                        G.add_edge(k, sk)
+    return G
+
+def plot_graph(topology, name='topology'):
+    G = create_graph(topology)
+    pos = nx.kamada_kawai_layout(G)
+    pprint(pos)
+    for node in G:
+        G.nodes[node]['pos'] = pos[node]
+        G.nodes[node]['text'] = node
+    fig = plot_g(G)
+    save(fig, name, w=2400, h=920)
+    return fig
+
 def sigmoid(x):
     return 1 / (1 + exp(-x))
 
 def test(x):
     return 1 / (1 + exp(-x))
 
+def logistic(w, x):
+    h = es('ij,jk->ik', w, x)
+    return sigmoid(h)
+
+def softmax(x):
+    top  = exp(x)
+    norm = es('ik->i', top)
+    return top / norm
+
+def sqrt(x):
+    return x ** 0.5
+
+def attention(keys, values, queries):
+    # Reference: http://einops.rocks/pytorch-examples.html
+    c = 2 # Hardcoded for now
+    attention = es('ct,cl->tl', keys, values)
+    attention = softmax(attention / sqrt(c)) # Scaled dot product attn
+    lookup    = es('ct,tl->cl', values, attention)
+    return lookup
+
 def main():
     test_trace = trace(test)
+    # test_trace = trace(attention)
     pprint(test_trace.topology)
+    plot_graph(test_trace.topology, 'attention_topology')
+    1/0
 
     n = 1000
     x = np.linspace(-10, 10, n)
